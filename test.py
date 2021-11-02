a03a0962d42e7698b223b3df04776e0c3166b13d
rfarwell <roryfarwell@outlook.com>
2021-11-02 20:40:09 +0000

    deleted some test files

--- test.py
+++
@@ -1,9 +0,0 @@
-import SimpleITK as sitk
-import numpy as np
-
-#reader = sitk.ImageFileReader()
-
-#reader.SetFileName("0-1.dcm")
-#reader.LoadPrivateTagsOn()
-#reader.ReadImageInformation()
-print("success")
