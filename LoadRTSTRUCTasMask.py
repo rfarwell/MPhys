a6a93a42ff0da8bb54388956d233e5542a6779b7
rfarwell <91542685+rfarwell@users.noreply.github.com>
2021-11-02 15:56:46 +0000

    Add files via upload
    Edited code to get information about the variable 'mask_3d' and how it looks.

--- LoadRTSTRUCTasMask.py
+++ LoadRTSTRUCTasMask.py
@@ -1,4 +1,6 @@
 import rt_utils
+import sys
+import numpy as np
 from rt_utils import RTStructBuilder
 import matplotlib.pyplot as plt
 
@@ -16,5 +18,30 @@ mask_3d = rtstruct.get_roi_mask_by_name("GTV-1") #Put in the name of the ROI
 
 # Display one slice of the region
 first_mask_slice = mask_3d[:, :, 80] #the number defines which slice will be displayed
-plt.imshow(first_mask_slice)
-plt.show()
+print(mask_3d.shape[2]) #prints the number of slices in the image (will be used when making an interactive plot)
+# plt.imshow(first_mask_slice)
+# plt.show()
+
+# for i in range(mask_3d.shape[2]) :
+#   plt.imshow(mask_3d[:,:,i])
+#   plt.show()
+
+with np.printoptions(threshold=sys.maxsize):
+     print(mask_3d)
+
+
+
+
+# with np.printoptions(threshold=sys.maxsize) :
+#   print(mask_3d[:,:,80].shape)
+
+# true_counter = 0
+# mask_test = mask_3d[:,:,80]
+# for row in mask_test:
+#     for cell in row:
+#         cell = str(cell)
+#         if cell == "True" :
+#           true_counter +=1
+#           print(true_counter)
+#           print("True")
+# print(true_counter)
