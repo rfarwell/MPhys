47592c3250db1738de2eb9c38f7010820d6dae1a
rfarwell <roryfarwell@outlook.com>
2021-11-09 17:03:38 +0000

    Update MASK_resample.py

--- MASK_resample.py
+++ MASK_resample.py
@@ -218,5 +218,9 @@ plt.show()
 #=============================================================================================
 
 #=========================== WRITING THE RESAMPLE MASK ARRAY AS A NIFTI FIL===================
+# sitk.WriteImage(mask_3d_image_resampled, "/Users/roryfarwell/Documents/University/Year4/MPhys/DataOrg/LUNG1-001/resampled/LUNG1-001-MASK-resampled.nii")
+# sitk.WriteImage(DICOM_resampled, "/Users/roryfarwell/Documents/University/Year4/MPhys/DataOrg/LUNG1-001/resampled/LUNG1-001-DICOM-resampled.nii")
+mask_3d_resampled = sitk.GetArrayFromImage(mask_3d_image_resampled)
+mask_3d_resampled.astype(np.float64)
 sitk.WriteImage(mask_3d_image_resampled, "/Users/roryfarwell/Documents/University/Year4/MPhys/DataOrg/LUNG1-001/resampled/LUNG1-001-MASK-resampled.nii")
-sitk.WriteImage(DICOM_resampled, "/Users/roryfarwell/Documents/University/Year4/MPhys/DataOrg/LUNG1-001/resampled/LUNG1-001-DICOM-resampled.nii")
+
