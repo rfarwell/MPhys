bcb678ec70b24c99e0bd1a3a55aaf748ae7877f4
rfarwell <roryfarwell@outlook.com>
2021-11-16 10:39:14 +0000

    Cleaning up by defining functions

--- MASK_resample_loop.py
+++ MASK_resample_loop.py
@@ -17,6 +17,8 @@ import SimpleITK as sitk
 import scipy
 from scipy import stats
 import os
+
+from MASK_resample_single import DICOM_resampled, DICOM_series_path
 reader = sitk.ImageSeriesReader()
 #================================================================================================
 
@@ -26,33 +28,21 @@ new_size = [512, 512, 512]
 #================================================================================================
 
 #========================== DEFINING FUNCTIONS ==================================================
-
-def resample_DICOM(volume, interpolator = sitk.sitkLinear, default_pixel_value = -1024) :
+def resample_volume(volume, interpolator, default_pixel_value) :
     '''
-    This function resample a volume to size 512 x 512 x 512 with spacing defined from Output_Spacing
+    This function resample a volume to size 512 x 512 x 512 with spacing 1 x 1 x 1.
+
+    It will be used in the resampling functions for both the DICOM series and the mask images.
+    To ensure they overlap well when put into a program such as Worldmatch the direction and origin of both
+    resampled volumes are set to the direction and origin of the original DICOM series.
+
+    Rory Farwell and Patrick Hastings (14/11/2021)
     '''
     resample = sitk.ResampleImageFilter()
     resample.SetInterpolator(interpolator)
     resample.SetOutputDirection(DICOM.GetDirection())
     resample.SetOutputOrigin(DICOM.GetOrigin())
     resample.SetSize(new_size)
-    resample.SetOutputSpacing([Output_Spacing[0], Output_Spacing[1], Output_Spacing[2]])
-    resample.SetDefaultPixelValue(default_pixel_value)
-
-    return resample.Execute(volume)
-
-
-def resample_MASK(volume, interpolator = sitk.sitkNearestNeighbor, default_pixel_value = 0) :
-    '''
-    This function resample a volume to size 512 x 512 x 256 with spacing 1 x 1 x 4 (Good for our dataset)
-    '''
-    print(volume.GetSize())
-    resample = sitk.ResampleImageFilter()
-    resample.SetInterpolator(interpolator)
-    resample.SetOutputDirection(DICOM.GetDirection())
-    resample.SetOutputOrigin(DICOM.GetOrigin())
-    resample.SetSize(new_size)
-    #resample.SetOutputSpacing([Output_Spacing[2], Output_Spacing[1], Output_Spacing[0]])
     resample.SetOutputSpacing([Output_Spacing[0], Output_Spacing[1], Output_Spacing[2]])
     resample.SetDefaultPixelValue(default_pixel_value)
 
@@ -62,11 +52,64 @@ def resample_MASK(volume, interpolator = sitk.sitkNearestNeighbor, default_pixel
 def permute_axes(volume, permutation_order) :
     """
     This function permutes the axes of the input volume.
+    It will be used on the mask because SimpleITK seems to flip the axes
+    at some stage in this process.
+
+    Patrick Hastings and Rory Farwell (16/11/2021) 
     """
     permute = sitk.PermuteAxesImageFilter()
     permute.SetOrder(permutation_order)
 
     return permute.Execute(volume)
+
+def resample_DICOM(interpolator = sitk.sitkLinear, default_pixel_value = -1024) :
+    """
+    This function will do the whole resampling process on the DICOM series and will make use
+    of the earlier defined resample_volume function.
+
+    Rory Farwell and Patrick Hastings (14/11/2021)
+    """
+    global DICOM #Means that DICOM will be defined globally
+    reader = sitk.ImageSeriesReader()
+    DICOM_paths = reader.GetGDCMSeriesFileNames(DICOM_series_path)
+    reader.SetFileNames(DICOM_paths)
+    DICOM = sitk.ReadImage(DICOM_paths)
+    DICOM_resampled = resample_volume(DICOM, interpolator, default_pixel_value) #DICOM_resampled is an Image/Object not an array
+
+    return DICOM_resampled
+
+def resample_MASK(interpolator = sitk.sitkNearestNeighbor, default_pixel_value = 0, Regions_of_Interest = ["Lung-Right", "Lung-Left"]) :
+    """
+    This function will perform the whole resampling process on a mask produced from an RTSTRUCT
+    file and will make use of the earlier defined resample_volume function.
+
+    Rory Farwell and Patrick Hastings (14/11/2021)
+    """
+    rtstruct = RTStructBuilder.create_from(DICOM_series_path, RTSTRUCT_path) # Telling the code where to get the DICOMs and RTSTRUCT from
+    
+    print(str(i) + rtstruct.get_roi_names()) # View all of the ROI names from within the image
+
+    #Getting arrays for all the masks for the determined ROIs
+    #Note that these are only the ROIs for LUNG1-001. Other patients may have different ROIs which is something I need to check.
+    mask_3d_Lung_Right = rtstruct.get_roi_mask_by_name("Lung-Right") 
+    mask_3d_Lung_Left = rtstruct.get_roi_mask_by_name("Lung-Left")
+    mask_3d_GTV_1 = rtstruct.get_roi_mask_by_name("GTV-1")
+    mask_3d_spinal_cord = rtstruct.get_roi_mask_by_name("Spinal-Cord")
+
+    mask_3d = mask_3d_Lung_Left + mask_3d_Lung_Right
+    
+    mask_3d = mask_3d.astype(np.float32) #Converting this array from boolean to float so that it can be converted to .nii file
+
+    mask_3d_image = sitk.GetImageFromArray(mask_3d) #Converting mask_3d array to an image
+
+    mask_3d_image = permute_axes(mask_3d_image, [1,2,0]) #permuting the axes of mask_2d because SimpleITK changes the axes ordering
+    mask_3d_image.SetSpacing(DICOM.GetSpacing())
+    mask_3d_image.SetDirection(DICOM.GetDirection())
+    mask_3d_image.SetOrigin(DICOM.GetOrigin())
+
+    mask_3d_image_resampled = resample_volume(mask_3d_image, interpolator, default_pixel_value)
+
+    return mask_3d_image_resampled
 
 #================================================================================================
 
@@ -78,54 +121,21 @@ filenumbers = filenumbers + 1
 filepath = '/Volumes/Extreme_SSD/MPhys/TCIA_Data/NSCLC-Radiomics/NSCLC_Sorted'
 
 for i in filenumbers :
-    CT_read_path = '/Volumes/Extreme_SSD/MPhys/TCIA_Data/NSCLC-Radiomics/NSCLC_Sorted/LUNG1-' + str('{0:03}'.format(i)) + '-CTUnknownStudyID'
-    RTSTRUCT_initial_read_path = '/Volumes/Extreme_SSD/MPhys/TCIA_Data/NSCLC-Radiomics/NSCLC_Sorted/LUNG1-' + str('{0:03}'.format(i)) + '-RTSTRUCTUnknownStudyID'
+    DICOM_series_path = '/Volumes/Extreme_SSD/MPhys/TCIA_Data/NSCLC-Radiomics/NSCLC_Sorted/LUNG1-' + str('{0:03}'.format(i)) + '-CTUnknownStudyID'
+    RTSTRUCT_initial_path = '/Volumes/Extreme_SSD/MPhys/TCIA_Data/NSCLC-Radiomics/NSCLC_Sorted/LUNG1-' + str('{0:03}'.format(i)) + '-RTSTRUCTUnknownStudyID'
     files_in_RTSTRUCT_folder = os.listdir('/Volumes/Extreme_SSD/MPhys/TCIA_Data/NSCLC-Radiomics/NSCLC_Sorted/LUNG1-' + str('{0:03}'.format(i)) + '-RTSTRUCTUnknownStudyID')
     RTSTRUCT_read_filename = str(files_in_RTSTRUCT_folder[0])
-    RTSTRUCT_read_path = RTSTRUCT_initial_read_path + '/' + RTSTRUCT_read_filename
+    RTSTRUCT_path = RTSTRUCT_initial_path + '/' + RTSTRUCT_read_filename
     
-    CT_write_path = '/Volumes/Extreme_SSD/MPhys/TCIA_Data/NSCLC-Radiomics/NSCLC_resampled_CT_and_RTSTRUCT/LUNG1-' + str('{0:03}'.format(i)) + '-CT.nii'
-    RTSTRUCT_write_path = '/Volumes/Extreme_SSD/MPhys/TCIA_Data/NSCLC-Radiomics/NSCLC_resampled_CT_and_RTSTRUCT/LUNG1-' + str('{0:03}'.format(i)) + '-RTSTRUCT.nii'
-    
-    reader = sitk.ImageSeriesReader()
-    dcm_paths = reader.GetGDCMSeriesFileNames(CT_read_path)
-    reader.SetFileNames(dcm_paths)
-    DICOM = sitk.ReadImage(dcm_paths)
-    DICOM_resampled = resample_DICOM(DICOM) #DICOM_resampled is an Image/Object not an array
-    sitk.WriteImage(DICOM_resampled, CT_write_path)
+    DICOM_write_path = '/Volumes/Extreme_SSD/MPhys/TCIA_Data/NSCLC-Radiomics/NSCLC_resampled_CT_and_RTSTRUCT/LUNG1-' + str('{0:03}'.format(i)) + '-CT.nii'
+    MASK_write_path = '/Volumes/Extreme_SSD/MPhys/TCIA_Data/NSCLC-Radiomics/NSCLC_resampled_CT_and_RTSTRUCT/LUNG1-' + str('{0:03}'.format(i)) + '-RTSTRUCT.nii'
 
-    rtstruct = RTStructBuilder.create_from(
-    dicom_series_path= CT_read_path, 
-    rt_struct_path= RTSTRUCT_read_path
-    )
-
-    print(str(i) + ": " + str(rtstruct.get_roi_names())) # View all of the ROI names from within the image
-
-    mask_3d_Lung_Right = rtstruct.get_roi_mask_by_name("Lung-Right") 
-    mask_3d_Lung_Left = rtstruct.get_roi_mask_by_name("Lung-Left")
-    mask_3d_GTV_1 = rtstruct.get_roi_mask_by_name("GTV-1")
-    mask_3d_spinal_cord = rtstruct.get_roi_mask_by_name("Spinal-Cord")
-
-    mask_3d = mask_3d_Lung_Right + mask_3d_Lung_Left
-
-    mask_3d = mask_3d.astype(np.float32)
-
-    mask_3d_image = sitk.GetImageFromArray(mask_3d)
-
-    mask_3d_image = permute_axes(mask_3d_image, [1,2,0])
-    mask_3d_image.SetSpacing(DICOM.GetSpacing())
-    mask_3d_image.SetDirection(DICOM.GetDirection())
-    mask_3d_image.SetOrigin(DICOM.GetOrigin())
-
-    mask_3d_image_resampled = resample_MASK(mask_3d_image, sitk.sitkNearestNeighbor, 0)
-    mask_3d_resampled = sitk.GetArrayFromImage(mask_3d_image_resampled)
-    mask_3d_resampled = mask_3d_resampled.astype(np.float32)
-    mask_3d_image_resampled = sitk.GetImageFromArray(mask_3d_resampled)
-    mask_3d_image_resampled.SetDirection(DICOM.GetDirection())
-    mask_3d_image_resampled.SetOrigin(DICOM.GetOrigin())
-    # print(mask_3d_image_resampled.GetOrigin())
-    sitk.WriteImage(mask_3d_image_resampled, RTSTRUCT_write_path)
+    DICOM_resampled = resample_DICOM()
+    mask_3d_image_resampled = resample_MASK()
+    sitk.WriteImage(mask_3d_image_resampled, MASK_write_path)
+    sitk.WriteImage(DICOM_resampled, DICOM_write_path)
 
     print('Completeted writing .nii files for CT and RTSTRUCT for file ' + str(i) + '.')
+    print("===========================================================================")
 
 
