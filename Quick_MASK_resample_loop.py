81d6e77a7a4815e057d0bd12e5ca426e65acc566
rfarwell <roryfarwell@outlook.com>
2021-11-18 12:12:00 +0000

    Create Quick_MASK_resample_loop.py

---
+++ Quick_MASK_resample_loop.py
@@ -0,0 +1,199 @@
+"""
+This code will use the ResampleImageFilter function to resample both the DICOM and Mask for a 
+relevant DICOM series. Also outputs both the DICOM and masks as .nii files which can be used 
+in 'worldmatch' to check that the mask and CT line up.
+
+BUT QUICKER
+
+Rory Farwell : Last Edited (17/11/2021) (dd/mm/yyyy)
+"""
+
+#========================== IMPORTING LIBRARIES =================================================
+from genericpath import getsize
+import rt_utils
+import sys
+import numpy as np
+from rt_utils import RTStructBuilder
+import matplotlib.pyplot as plt
+import SimpleITK as sitk
+import scipy
+from scipy import stats
+import os
+
+from MASK_resample_single import DICOM_resampled, DICOM_series_path
+reader = sitk.ImageSeriesReader()
+#================================================================================================
+
+#========================== DEFINING RESAMPLING VARIABLES =======================================
+Output_Spacing = [1, 1, 6]
+new_size = [512, 512, 128]
+#================================================================================================
+
+#========================== DEFINING FUNCTIONS ==================================================
+
+def resample_volume(volume, interpolator, default_pixel_value) :
+    '''
+    This function resample a volume to size 512 x 512 x 512 with spacing 1 x 1 x 1.
+
+    It will be used in the resampling functions for both the DICOM series and the mask images.
+    To ensure they overlap well when put into a program such as Worldmatch the direction and origin of both
+    resampled volumes are set to the direction and origin of the original DICOM series.
+
+    Rory Farwell and Patrick Hastings (14/11/2021)
+    '''
+    resample = sitk.ResampleImageFilter()
+    resample.SetInterpolator(interpolator)
+    resample.SetOutputDirection(DICOM.GetDirection())
+    resample.SetOutputOrigin(DICOM.GetOrigin())
+    resample.SetSize(new_size)
+    resample.SetOutputSpacing([Output_Spacing[0], Output_Spacing[1], Output_Spacing[2]])
+    resample.SetDefaultPixelValue(default_pixel_value)
+
+    return resample.Execute(volume)
+
+def permute_axes(volume, permutation_order) :
+    """
+    This function permutes the axes of the input volume.
+    It will be used on the mask because SimpleITK seems to flip the axes
+    at some stage in this process.
+
+    Patrick Hastings and Rory Farwell (16/11/2021) 
+    """
+    permute = sitk.PermuteAxesImageFilter()
+    permute.SetOrder(permutation_order)
+
+    return permute.Execute(volume)
+
+def resample_DICOM(patient_number, interpolator = sitk.sitkLinear, default_pixel_value = -1024) :
+    """
+    This function will do the whole resampling process on the DICOM series and will make use
+    of the earlier defined resample_volume function.
+
+    Rory Farwell and Patrick Hastings (14/11/2021)
+    """
+    DICOM_write_path = '/Volumes/Extreme_SSD/MPhys/TCIA_Data/NSCLC-Radiomics/2NSCLC_resampled_CT_and_RTSTRUCT/LUNG1-' + str('{0:03}'.format(patient_number)) + '-CT.nii'
+
+    DICOM_paths = reader.GetGDCMSeriesFileNames(DICOM_series_path)
+    reader.SetFileNames(DICOM_paths)
+    global DICOM #Means that DICOM will be defined globally
+    DICOM = sitk.ReadImage(DICOM_paths)
+    DICOM_resampled = resample_volume(DICOM, interpolator, default_pixel_value) #DICOM_resampled is an Image/Object not an array
+
+    return sitk.WriteImage(DICOM_resampled, DICOM_write_path)
+
+def resample_MASKS(patient_number, interpolator = sitk.sitkNearestNeighbor, default_pixel_value = 0) :
+    """
+    This function will perform the whole resampling process on a mask produced from an RTSTRUCT
+    file and will make use of the earlier defined resample_volume function.
+
+    Rory Farwell and Patrick Hastings (14/11/2021)
+    """
+    GTV_1_MASK_write_path = '/Volumes/Extreme_SSD/MPhys/TCIA_Data/NSCLC-Radiomics/2NSCLC_resampled_CT_and_RTSTRUCT/LUNG1-' + str('{0:03}'.format(patient_number)) + '-GTV-1.nii'
+    ALL_GTV_MASK_write_path = '/Volumes/Extreme_SSD/MPhys/TCIA_Data/NSCLC-Radiomics/2NSCLC_resampled_CT_and_RTSTRUCT/LUNG1-' + str('{0:03}'.format(patient_number)) + '-ALL_GTV.nii'
+    
+    #==============================================================
+    rtstruct = RTStructBuilder.create_from(DICOM_series_path, RTSTRUCT_path) # Telling the code where to get the DICOMs and RTSTRUCT from
+    mask_3d = np.zeros(DICOM.GetSize(), dtype = bool)
+    mask_3d += rtstruct.get_roi_mask_by_name("GTV-1")
+    mask_3d = mask_3d.astype(np.float32) #Converting this array from boolean to float so that it can be converted to .nii file
+    mask_3d_image = sitk.GetImageFromArray(mask_3d) #Converting mask_3d array to an image
+
+    mask_3d_image = permute_axes(mask_3d_image, [1,2,0]) #permuting the axes of mask_2d because SimpleITK changes the axes ordering
+    mask_3d_image.SetSpacing(DICOM.GetSpacing())
+    mask_3d_image.SetDirection(DICOM.GetDirection())
+    mask_3d_image.SetOrigin(DICOM.GetOrigin())
+
+    mask_3d_image_resampled = resample_volume(mask_3d_image, interpolator, default_pixel_value)
+
+    sitk.WriteImage(mask_3d_image_resampled, GTV_1_MASK_write_path)
+    #==============================================================
+    
+    mask_3d = np.zeros(DICOM.GetSize(), dtype = bool)
+
+    print(ROIs) #Printing to check the for loop below is doing the right thing.
+
+    for ROI in ROIs :
+        mask_3d_temp = rtstruct.get_roi_mask_by_name(str(ROI))
+        if "GTV" in ROI or "gtv" in ROI :
+            mask_3d = mask_3d + mask_3d_temp
+            print(ROI)
+    
+    mask_3d = mask_3d.astype(np.float32) #Converting this array from boolean to float so that it can be converted to .nii file
+
+    mask_3d_image = sitk.GetImageFromArray(mask_3d) #Converting mask_3d array to an image
+
+    mask_3d_image = permute_axes(mask_3d_image, [1,2,0]) #permuting the axes of mask_2d because SimpleITK changes the axes ordering
+    mask_3d_image.SetSpacing(DICOM.GetSpacing())
+    mask_3d_image.SetDirection(DICOM.GetDirection())
+    mask_3d_image.SetOrigin(DICOM.GetOrigin())
+
+    mask_3d_image_resampled = resample_volume(mask_3d_image, interpolator, default_pixel_value)
+
+    sitk.WriteImage(mask_3d_image_resampled, ALL_GTV_MASK_write_path)
+
+    return
+
+def opening_test(patient_number) :
+    """
+    Tries to open both the DICOM series and RTSTRUCT
+    """
+
+#=====================================================================================================================
+number_of_iterations = 10
+filenumbers = np.arange(number_of_iterations)
+filenumbers = filenumbers + 1
+
+for i in filenumbers :
+    DICOM_series_path = '/Volumes/Extreme_SSD/MPhys/TCIA_Data/NSCLC-Radiomics/NSCLC_Sorted/LUNG1-' + str('{0:03}'.format(i) + '-CT')
+    RTSTRUCT_initial_path = '/Volumes/Extreme_SSD/MPhys/TCIA_Data/NSCLC-Radiomics/NSCLC_Sorted/LUNG1-' + str('{0:03}'.format(i) + '-RTSTRUCT')
+    files_in_RTSTRUCT_folder = os.listdir('/Volumes/Extreme_SSD/MPhys/TCIA_Data/NSCLC-Radiomics/NSCLC_Sorted/LUNG1-' + str('{0:03}'.format(i)) + '-RTSTRUCT')
+    RTSTRUCT_filename = files_in_RTSTRUCT_folder[0]
+    RTSTRUCT_read_filename = str(RTSTRUCT_filename)
+    RTSTRUCT_path = RTSTRUCT_initial_path + '/' + RTSTRUCT_read_filename
+    
+    try :
+        """
+        Try to read both the RTSTRUCT and DICOM series.
+        """
+        rtstruct = RTStructBuilder.create_from(DICOM_series_path, RTSTRUCT_path)
+        DICOM_paths = reader.GetGDCMSeriesFileNames(DICOM_series_path)
+        reader.SetFileNames(DICOM_paths)
+        DICOM = sitk.ReadImage(DICOM_paths)  
+        
+        ROIs = rtstruct.get_roi_names()
+    
+        for ROI in ROIs :
+            str(ROI)
+            if "pre-op" in ROI :
+                print('The ROIs for this patient included the gtv-preop so has been skipped')
+                continue
+
+        resample_DICOM(patient_number = i)
+        resample_MASKS(patient_number = i)
+
+        print('Completed writing files for LUNG1-' + str('{0:03}'.format(i)) +'.')       
+
+    except :
+        """
+        If unable to read in both RTSTRUCT and DICOM series.
+        """
+        print('Unable to read either RTSTRUCT or DICOM series for LUNG1-' + str('{0:03}'.format(i)) + 'so it has been skipped.')
+        continue # if the RTSTRUCT or DICOM can't be opened then this 'i' is skipped
+
+print(len(os.listdir('/Volumes/Extreme_SSD/MPhys/TCIA_Data/NSCLC-Radiomics/2NSCLC_resampled_CT_and_RTSTRUCT')))
+
+
+    # ROIs = rtstruct.get_roi_names()
+    
+    # for ROI in ROIs :
+    #     str(ROI)
+    #     if "pre-op" in ROI :
+    #         print('The ROIs for this patient included the gtv-preop so has been skipped')
+    #         continue
+
+    # resample_DICOM(patient_number = i)
+    # resample_MASKS(patient_number = i)
+
+    # print('Completed writing files for LUNG1-' + str('{0:03}'.format(i)) +'.')
+
+print("===========================================================================")
